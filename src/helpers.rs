1fe6c416e057437d629da19b0d5718864da690db
rouzwelt <rouzweltt@gmail.com>
2025-02-07 05:07:18 +0000

    update

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,20 +1,18 @@
-use std::str::FromStr;
+use alloy_primitives::{U256, ruint::ParseError};
+use std::{error::Error, fmt::Display, str::FromStr};
 
-// A trait for converting types to U256
-pub trait TryIntoU256 {
-    type Error;
-    fn try_into_u256(&self) -> Result<alloy::primitives::U256, Self::Error>;
-}
-
-// implemented for js bigint to convert to alloy U256
-impl TryIntoU256 for js_sys::BigInt {
-    type Error = alloy::primitives::ruint::ParseError;
-    fn try_into_u256(&self) -> Result<alloy::primitives::U256, Self::Error> {
-        alloy::primitives::U256::from_str(&format!("{}", &self))
+// A trait for converting a given type to U256
+pub trait TryIntoU256<T: FromStr<Err = E>, E: Error>
+where
+    Self: Display,
+{
+    fn try_into_u256(&self) -> Result<T, E> {
+        T::from_str(&format!("{}", &self))
     }
 }
+impl TryIntoU256<U256, ParseError> for js_sys::BigInt {}
 
-// a serializer fn for serializing Vec<u8> as Uint8Array for js
+// a serializer fn for serializing Vec<u8> as bytes (Uint8Array for js)
 pub fn bytes_serilializer<S: serde::Serializer>(
     val: &[u8],
     serializer: S,
@@ -33,7 +31,7 @@ mod tests {
     fn test_js_bigint_to_u256() {
         let bigint = js_sys::BigInt::from_str("12").unwrap();
         let result = bigint.try_into_u256().unwrap();
-        let expected = alloy::primitives::U256::from(12);
+        let expected = U256::from(12);
         assert_eq!(result, expected);
     }
 
