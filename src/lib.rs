3dba6d55a0d92bfe7297af45e6a1b78e2a91cf50
rouzwelt <rouzweltt@gmail.com>
2025-02-11 00:13:59 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,47 @@
-//! Provides utilities, helpers and macros to easily build and customize [wasm_bindgen] bindings.
+//! Provides utilities, helpers and macros to easily build and customize [wasm_bindgen] bindings,
+//! such as [impl_wasm_traits] macro that will implement wasm traits for a give type and
+//! [serialize_hashmap_as_object] serializer function to serialize a hashmap as object used with
+//! serde `serialize_with` attribute.
+//!
+//! Example:
+//! ```ignore
+//! #[derive(Serialize, Deserialize)]
+//! #[serde(rename_all = "camelCase")]
+//! pub struct SomeType {
+//!     #[cfg_attr(target_family = "wasm", serde(serialize_with = "bytes_serilializer"))]
+//!     pub field: Vec<u8>,
+//!     #[cfg_attr(target_family = "wasm", serde(serialize_with = "serialize_hashmap_as_object"))]
+//!     pub other_field: HashMap<String, u8>,
+//! }
+//!
+//! // impl wasm traits for SomeType
+//! impl_wasm_traits!(SomeType);
+//!
+//! // impl tsify manually for SomeType
+//! // the given string literal will become the typescript interface bindings for SomeType
+//! impl_custom_tsify!(
+//!     SomeType,
+//!     "export interface SomeType {
+//!         field: Uint8Array;
+//!         otherField: Record<string, number>;
+//!     }"
+//! );
+//!
+//! // now someType can be used on functions and methods natively
+//! #[wasm_bindgen]
+//! pub fn some_fn(arg: SomeType) -> String {
+//!     // body
+//! }
+//!
+//! #[wasm_bindgen]
+//! pub async fn some_other_fn(arg1: Vec<u8>, arg2: HashMap<String, u8>) -> Result<SomeType, Error> {
+//!     // body
+//!     Ok(SomeType {
+//!         field: arg1,
+//!         other_field: arg2
+//!     })
+//! }
+//! ```
 
 mod ser;
 pub mod macros;
