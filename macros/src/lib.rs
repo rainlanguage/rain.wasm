05f2e1ad908e2532148bd3aeeb6e3c48effae1a0
rouzwelt <rouzweltt@gmail.com>
2025-03-07 16:18:39 +0000

    docs

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,6 +2,71 @@ use proc_macro::TokenStream;
 
 mod wasm_export;
 
+/// A proc macro that generates the wasm function bindings with "WasmEncodedResult"
+/// return type from rust functions that natively return [Result<T, E>], this makes
+/// it possible to avoid throwing on js when return value is [Result::Err] variant,
+/// instead it will return "WasmEncodedResult<T>" normally on js where either of
+/// [Result::Ok] or [Result::Err] varinats are included within the "WasmEcnodedResult"
+/// properties.
+/// All of the "wasm_bindgen" attributes are available for this macro and are forwarded
+/// directly to "wasm_bindgen" macro on expansion.
+///
+/// Example:
+/// ```ignore
+/// use wasm_bindgen_utils::prelude::*;
+///
+/// struct TestStruct;
+///
+/// #[wasm_export]
+/// impl TestStruct {
+///     #[wasm_export(js_name = "someStaticMethod", unchecked_return_type = "string")]
+///     pub async fn some_static_method((arg1, arg2): (String, u8)) -> Result<String, Error> {
+///         Ok(String::new())
+///     }
+///     #[wasm_export(skip)]
+///     pub async fn some_skip_fn() -> Result<String, Error> {
+///         Ok(String::new())
+///     }
+///     #[wasm_export(some_other_wbg_attrs)]
+///     pub fn some_self_method(&self, arg: String) -> Result<TestStruct, Error> {
+///         Ok(TestStruct)
+///     }
+/// }
+/// ```
+/// above will translate to the following:
+/// ```ignore
+/// impl TestStruct {
+///     pub async fn some_static_method(
+///         (arg1, arg2): (String, u8),
+///     ) -> Result<String, Error> {
+///         Ok(String::new())
+///     }
+///     pub async fn some_skip_fn() -> Result<String, Error> {
+///         Ok(String::new())
+///     }
+///     pub fn some_self_method(&self, arg: String) -> Result<TestStruct, Error> {
+///         Ok(TestStruct)
+///     }
+/// }
+/// #[wasm_bindgen]
+/// impl TestStruct {
+///     #[wasm_bindgen(js_name = "someStaticMethod")]
+///     #[wasm_bindgen(unchecked_return_type = "WasmEncodedResult<string>")]
+///     pub async fn some_static_method__wasm_export(
+///         (arg1, arg2): (String, u8),
+///     ) -> WasmEncodedResult<String> {
+///         Self::some_static_method((arg1, arg2)).await.into()
+///     }
+///     #[wasm_bindgen(some_other_wbg_attrs)]
+///     #[wasm_bindgen(unchecked_return_type = "WasmEncodedResult<TestStruct>")]
+///     pub fn some_self_method__wasm_export(
+///         &self,
+///         arg: String,
+///     ) -> WasmEncodedResult<TestStruct> {
+///         self.some_self_method(arg).into()
+///     }
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn wasm_export(attr: TokenStream, item: TokenStream) -> TokenStream {
     match wasm_export::expand(attr, item) {
--- src/result.rs
+++ src/result.rs
@@ -2,6 +2,11 @@ use tsify::Tsify;
 use crate::impl_wasm_traits;
 use serde::{Serialize, Deserialize};
 
+/// A struct that holds info of a rust error that is serializable
+/// natively to js through wasm bindgen, so [Result::Err] variants
+/// of binding functions can return normaly in js instead of throwing.
+///
+/// Rust errors should impl [Into]/[From] trait to this struct.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Tsify)]
 #[serde(rename_all = "camelCase")]
 pub struct WasmEncodedError {
@@ -10,6 +15,16 @@ pub struct WasmEncodedError {
 }
 impl_wasm_traits!(WasmEncodedError);
 
+/// A generic struct that either holds info of a rust [Result] that
+/// is serializable natively to js through wasm bindgen, so binding
+/// functions can return normaly in js instead of throwing by holding
+/// either [Result::Ok] variant in its "value" prop or [Result::Err]
+/// in its "error" prop.
+/// Used in [wasm_bindgen_utils_macros::wasm_export] as the returning
+/// type of exporting wasm binding functions.
+///
+/// [From] trait has been implemented for any [Result<T, E>] where
+/// "E" implements [Into<WasmEncodedError>].
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Tsify)]
 pub struct WasmEncodedResult<T> {
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -22,12 +37,14 @@ pub struct WasmEncodedResult<T> {
 impl_wasm_traits!(WasmEncodedResult<T>);
 
 impl<T> WasmEncodedResult<T> {
+    /// Creates a success instance from the given type
     pub fn success(value: T) -> Self {
         WasmEncodedResult {
             value: Some(value),
             error: None,
         }
     }
+    /// Creates a success instance from the given WasmEncodedError
     pub fn error(err: WasmEncodedError) -> Self {
         WasmEncodedResult {
             value: None,
