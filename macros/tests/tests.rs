ca4832b71676bd9662fc3e00d76d12803864e5bb
rouzwelt <rouzweltt@gmail.com>
2025-03-08 21:20:30 +0000

    update

--- macros/tests/tests.rs
+++ macros/tests/tests.rs
@@ -2,7 +2,7 @@
 pub fn happy() {
     // to update the tests "*.expanded.rs" fixtures, first
     // delete them and then run cargo test to regenerate them
-    macrotest::expand("tests/happy/*.rs");
+    macrotest::expand("tests/happy/*.test.rs");
 }
 
 #[test]
@@ -10,5 +10,5 @@ fn unhappy() {
     // to update the tests "*.stderr" fixtures, set
     // env TRYBUILD=overwrite and then run cargo test
     let test_runner = trybuild::TestCases::new();
-    test_runner.compile_fail("tests/unhappy/*.rs");
+    test_runner.compile_fail("tests/unhappy/*.test.rs");
 }
---
+++ tests/unhappy/a.test.rs
@@ -0,0 +1,7 @@
+#[macro_use]
+extern crate wasm_bindgen_utils;
+
+
+impl_wasm_traits!(SomeType);
+
+fn main() {}
